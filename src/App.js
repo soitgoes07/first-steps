a1e8591b6bd05a6e5807e700f2f99db32627d63c
aaron <abeaman95@gmail.com>
2022-04-21 15:34:41 -0400

    setting up front end website layout and possible package fix private is false

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      message: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const inputValue = event.target.value;
+    const stateField = event.target.name;
+    this.setState({
+      [stateField]: inputValue,
+    });
+    console.log(this.state);
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    const { name, message } = this.state;
+    await axios.post(
+      'https://i1xsjzkri4.execute-api.us-east-1.amazonaws.com/default/serverlessAppFunction',
+      { key1: `${name}, ${message}` }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+
+          <label>Message:</label>
+          <input
+            type="text"
+            name="message"
+            onChange={this.handleChange}
+            value={this.state.message}
+          />
+
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
